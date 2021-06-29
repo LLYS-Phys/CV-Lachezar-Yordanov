01ffee265729067625546b435d4448880b5972de
LLYS-Phys <lachyordanov@gmail.com>
2021-06-29 20:59:06 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ function navigmenu() {
   }
 
   function readmore() {
-    var readmore = document.getElementById("readmore");
+    var readmore = document.getElementById("learnmore");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
   
@@ -20,5 +20,69 @@ function navigmenu() {
       readmore.style.display = "none";
       btnText.innerHTML = "Read less";
       moreText.style.display = "inline";
+    }
+  }
+
+  function readmore2() {
+    var readmore2 = document.getElementById("learnmore2");
+    var moreText2 = document.getElementById("more2");
+    var btnText2 = document.getElementById("myBtn2");
+  
+    if (readmore2.style.display === "none") {
+      readmore2.style.display = "inline";
+      btnText2.innerHTML = "Read more";
+      moreText2.style.display = "none";
+    } else {
+      readmore2.style.display = "none";
+      btnText2.innerHTML = "Read less";
+      moreText2.style.display = "inline";
+    }
+  }
+
+  function readmore3() {
+    var readmore3 = document.getElementById("learnmore3");
+    var moreText3 = document.getElementById("more3");
+    var btnText3 = document.getElementById("myBtn3");
+  
+    if (readmore3.style.display === "none") {
+      readmore3.style.display = "inline";
+      btnText3.innerHTML = "Read more";
+      moreText3.style.display = "none";
+    } else {
+      readmore3.style.display = "none";
+      btnText3.innerHTML = "Read less";
+      moreText3.style.display = "inline";
+    }
+  }
+
+  function readmore4() {
+    var readmore4 = document.getElementById("learnmore4");
+    var moreText4 = document.getElementById("more4");
+    var btnText4 = document.getElementById("myBtn4");
+  
+    if (readmore4.style.display === "none") {
+      readmore4.style.display = "inline";
+      btnText4.innerHTML = "Read more";
+      moreText4.style.display = "none";
+    } else {
+      readmore4.style.display = "none";
+      btnText4.innerHTML = "Read less";
+      moreText4.style.display = "inline";
+    }
+  }
+
+  function readmore5() {
+    var readmore5 = document.getElementById("learnmore5");
+    var moreText5 = document.getElementById("more5");
+    var btnText5 = document.getElementById("myBtn5");
+  
+    if (readmore5.style.display === "none") {
+      readmore5.style.display = "inline";
+      btnText5.innerHTML = "Read more";
+      moreText5.style.display = "none";
+    } else {
+      readmore5.style.display = "none";
+      btnText5.innerHTML = "Read less";
+      moreText5.style.display = "inline";
     }
   }
