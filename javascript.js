90f0eaa5870a6a2512858b7713d55cc80a08076d
LLYS-Phys <lachyordanov@gmail.com>
2021-06-29 17:38:15 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -6,3 +6,19 @@ function myFunction() {
       x.className = "navig";
     }
   }
+
+  function myFunction() {
+    var readmore = document.getElementById("readmore");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (readmore.style.display === "none") {
+      readmore.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      readmore.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
